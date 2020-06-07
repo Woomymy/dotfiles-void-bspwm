39c8d4e767a8678ac243d6ea82bbd31a2450c7c6
= <=>
2020-06-07 11:50:34 +0200

    Portage

--- copy.sh
+++ copy.sh
@@ -12,11 +12,34 @@ mkdir backup
 
 makedirs() {
     cd ./backup 
+    dirs=(portage node  config)
+    for direc in "${dirs[*]}" 
+    do 
+        mkdir ${direc}
+    done
+    cd ..
+}
+copyportage() {
+    echo "Copying portage configuration";
+    cd backup/portage
+    packagesdot=(accept_keywords mask use unmask license keywords) #All package.${name} directories 
+    for direc in ${packagesdot[*]}
+    do 
+        if [ -d "/etc/portage/package.${direc}" ]; then 
+            cp -r /etc/portage/package.${direc} .
+        elif [ -r "/etc/portage/package.${direc}" ]; then 
+           cp /etc/portage/package.${direc} .  
+        elif [ -f "/etc/portage/package.${direc}" ]; then 
+            echo "Unable to read file /etc/portage/package.${direc}"
+        fi
+    done
 
 }
 
 main() {
     copydir
+    makedirs
+    copyportage
 }
 
 
