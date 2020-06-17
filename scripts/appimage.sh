ce6fdf355408dea15259917608f087d391167673
Woomymy <github@woomy.ovh>
2020-06-17 11:19:21 +0200

    Backup mercredi 17 juin 11:19  2020

---
+++ scripts/appimage.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+#Script permettant désarchiver une appimage pour avoir un gain de performance (plus ou moins gros). 
+echo "Ou se trouve l'appimage?"
+read path 
+resolepath() {
+
+}
