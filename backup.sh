4285a1d81b47849f3bf0bea1adc2677fa322f1b2
Woomymy <github@woomy.ovh>
2020-11-11 15:34:53 +0100

    remove old dirs

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ ! -f "./lib/functions.sh" && ! -f "./lib/colors.sh" ]]; then
+if [[ ! -f "./lib/functions.sh" || ! -f "./lib/copy.sh" || ! -f "./lib/colors.sh" ]]; then
     echo -e "\e[91mError! The functions files aren't present"
 fi
 source "./lib/functions.sh"
--- lib/functions.sh
+++ lib/functions.sh
@@ -1,8 +1,19 @@
 source "./lib/colors.sh"
 banner() {
-    cyanprint "=============="
-    cyanprint "=============="
+    cyanprint "==================="
+    cyanprint "Copying dotfiles..."
+    cyanprint "==================="
+}
+clean_dirs() {
+    DIRS=(home etc) 
+    for DIR in ${DIRS[*]}; do 
+        if [ -d "./${DIR}" ]
+        then
+            rm -rf "./${DIR}"
+        fi
+    done
 }
 main() {
     banner # Execute the banner
+    clean_dirs # Cleans old directories
 }
