d6a7c44ed53ee45cb77516c582ef6b08357d84f6
Woomy <woomy@woomy.be>
2020-11-11 15:24:09 +0100

    Added color print funcions

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-if [[ ! -f "./lib/functions.sh" ]]; then
-    echo -e "\e[91mError! The functions.sh file isn't present"
+if [[ ! -f "./lib/functions.sh" && ! -f "./lib/colors.sh" ]]; then
+    echo -e "\e[91mError! The functions files aren't present"
 fi
 source "./lib/functions.sh"
+main
