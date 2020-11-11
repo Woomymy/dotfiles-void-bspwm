0cca5620b2bda749e1f948af524cafb365a2795b
Woomymy <github@woomy.ovh>
2020-11-11 15:58:57 +0100

    mercredi 11 novembre 15:58  2020

--- lib/functions.sh
+++ lib/functions.sh
@@ -15,10 +15,17 @@ clean_dirs() {
     done
 }
 copy_files() {
-    
+    echo "Hello"
+}
+commit_and_push() {
+    greenprint "Commiting to git"
+    git add .
+    git commit -m "$(date +"%A %d %B %R  %Y")" # Format like this: DAY date HOUR:MINUTE YEAR
+    git push origin main
 }
 main() {
     banner # Execute the banner
     clean_dirs # Cleans old directories
     copy_files # Copy the files
+    commit_and_push # Commit and push to github
 }
