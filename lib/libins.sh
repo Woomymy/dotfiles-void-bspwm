2c5072ed70951d586de5e190a8ab57c401d030a6
Woomy <woomy@woomy.be>
2020-11-13 19:48:44 +0100

    vendredi 13 novembre 19:48  2020

--- lib/libins.sh
+++ lib/libins.sh
@@ -3,5 +3,7 @@ source "./lib/insfuncs.sh"
 source "./lib/inschecks.sh"
 main() {
 	cyanprint "Installing dotfiles..."
+	cyanprint "Running checks..."
 	run_syschecks
+	greenprint "All checks OK!"
 }
