179fbe45bde28b0e0f489ea6136d6ec7f3592bda
Woomy <woomy@woomy.be>
2020-11-13 18:20:09 +0100

    Check if this is a calculate linux

--- lib/libins.sh
+++ lib/libins.sh
@@ -1,4 +1,9 @@
 source "./lib/colors.sh"
 main() {
-	cyanprint "Hello"
+	cyanprint "Installing dotfiles..."
+	if [[ ! "$(command -v "cl-install")" ]]
+	then
+		redprint "This system isn't a calculate Linux System!"
+		exit 1
+	fi
 }
