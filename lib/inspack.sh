f8d491a32d44ff8a0a230dea05b05e2d948f5606
Woomymy <github@woomy.ovh>
2020-11-16 17:05:28 +0100

    backup

--- lib/inspack.sh
+++ lib/inspack.sh
@@ -1,6 +1,6 @@
 source "lib/colors.sh"
 if [[ "$(grep $1 "/var/lib/portage/world")" ]]; then
-	exit # Do not re-emerge packages if they are already installed
+	exit 
 fi
 greenprint "Installing packages ${1}"
 echo "${1}" >> "tmp/packagelist"
